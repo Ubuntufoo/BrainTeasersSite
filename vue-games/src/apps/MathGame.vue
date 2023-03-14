27718001f0d512241a7268588158809a7fc33eeb
Timothy Murphy <Timmurphy123@gmail.com>
2023-03-14 14:31:53 -0400

    Templates reverted to "instructions only"

---
+++ vue-games/src/apps/MathGame.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>This is the Math Game Vue.</p>
+</template>
--- vue-games/src/main.js
+++ vue-games/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router';
 
-createApp(App).mount('#app')
+import App from "./App";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
