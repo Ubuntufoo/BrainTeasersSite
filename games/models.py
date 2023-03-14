3e3d74b749ecd9026dcce369fc7bacdda4b88e44
Timothy Murphy <Timmurphy123@gmail.com>
2023-03-14 15:27:35 -0400

    GameScore model/url/view

--- games/models.py
+++ games/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class GameScore(models.Model):
+
+    MATH = "MATH"
+    ANAGRAM = "ANAGRAM"
+
+    GAME_CHOICES = [
+        (MATH, "Math Game"),
+        (ANAGRAM, "Anagram Game")
+    ]
+
+    user_name = models.TextField()
+    game = models.TextField()
+    score = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
--- games/views.py
+++ games/views.py
@@ -1,4 +1,25 @@
+import json
+from django.http import JsonResponse
+from games.models import GameScore
+
 from django.views.generic import TemplateView
+
+
+def record_score(request):
+    data = json.loads(request.body)
+
+    user_name = data["user-name"]
+    game = data["game"]
+    score = data["score"]
+
+    new_score = GameScore(user_name=user_name, game=game, score=score)
+    new_score.save()
+
+    response = {
+        "success": True
+    }
+
+    return JsonResponse(response)
 
 
 class HomeView(TemplateView):
