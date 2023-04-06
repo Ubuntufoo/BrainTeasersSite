669a2a28465af7c3acaae86bac4a8c092e065741
Timothy Murphy <Timmurphy123@gmail.com>
2023-04-06 13:06:42 -0400

    updated project config files

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,7 +1,7 @@
-//const { defineConfig } = require('@vue/cli-service')
-//module.exports = defineConfig({
-//  transpileDependencies: true
-//})
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
 
 module.exports = {
   publicPath: 'http://localhost:8080', // The base URL where your app will be deployed
