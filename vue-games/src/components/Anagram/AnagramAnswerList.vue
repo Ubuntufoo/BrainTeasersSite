78196de99c6d8091c9ace3e5d89539bec0750545
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-24 13:40:41 -0400

    hover effects, minor home page tweaks

--- vue-games/src/components/Anagram/AnagramAnswerList.vue
+++ vue-games/src/components/Anagram/AnagramAnswerList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="vstack my-5">
+  <div class="mx-auto vstack my-5">
   <transition-group name="list" tag="div">
     <li v-for="item in answers" :key="item" :class="'fs-1 fw-bold text-white rounded-pill bg-success text-center w-75 my-1 mx-auto'">
       {{ item }}
