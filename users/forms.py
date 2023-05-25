c751080fc04511fd42c83eafb02601c956974c5d
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-25 16:46:47 -0400

    added avatars to model/urls/template

--- users/forms.py
+++ users/forms.py
@@ -22,7 +22,8 @@ class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username', 'first_name', 'last_name', 'dob')
+        fields = ('email', 'username', 'first_name',
+                  'last_name', 'dob', 'avatar')
         widgets = {
             'dob': forms.SelectDateWidget(
                 attrs={
