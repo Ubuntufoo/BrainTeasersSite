86c472c32e21372dea4db55027b25c70098b3577
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-25 21:48:44 -0400

    secret key reverted

--- mydjangosite/settings.py
+++ mydjangosite/settings.py
@@ -22,9 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = '_9#q*hq&=y4hcsg5m5tt-v(4uxda(_8%1ace1(&j13+u!drzis'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
