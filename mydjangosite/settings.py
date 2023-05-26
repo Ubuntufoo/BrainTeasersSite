cbbaf912e5464272b42700f3c8b7f55ab343fba0
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-25 22:06:32 -0400

    update to secret key (var set on heroku)

--- mydjangosite/settings.py
+++ mydjangosite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_9#q*hq&=y4hcsg5m5tt-v(4uxda(_8%1ace1(&j13+u!drzis'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
