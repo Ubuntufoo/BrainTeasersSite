05adb0c2d9c409fcba838b036c3f27ded632aece
Timothy Murphy <99925702+Ubuntufoo@users.noreply.github.com>
2023-05-27 19:26:05 -0400

    Update vue.config.js output dir edit

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: 'http://localhost:8080', // The base URL where your app will be deployed
-  outputDir: '../static/dist', // The path for where files will be output when the app is built
+  outputDir: '../static', // The path for where files will be output when the app is built
   indexPath: '../../templates/_base_vue.html', // The path for the generated index file
 
   configureWebpack: {
