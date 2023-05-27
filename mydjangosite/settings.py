6d3a53d478a805813e2761d668131d457af44141
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-27 17:31:57 -0400

    removed STATICFILES_STORAGE =

--- mydjangosite/settings.py
+++ mydjangosite/settings.py
@@ -176,7 +176,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = 'Play2LearnFinal\static'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
