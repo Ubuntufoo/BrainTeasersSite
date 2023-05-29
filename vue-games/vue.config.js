419e8207dbda954e80183105b6ccce943ae39e06
Timothy Murphy <99925702+Ubuntufoo@users.noreply.github.com>
2023-05-29 10:34:27 -0400

    Update vue.config.js

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // publicPath: 'http://localhost:8080', // The base URL where your app will be deployed
-  publicPath: 'brainteasers.herokuapp.com', // The base URL where your app will be deployed
+  publicPath: 'brainteasers.herokuapp.com/', // The base URL where your app will be deployed
   indexPath: '../../templates/_base_vue.html', // The path for the generated index file
 
   configureWebpack: {
