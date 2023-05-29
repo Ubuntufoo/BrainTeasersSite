a27154163101fe0a79bfae152cc48d5628d7ddc8
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-29 12:34:28 -0400

    bg for header; docu for vue config settings

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: 'static/dist/', // The base URL where your app will be deployed
+  publicPath: 'static/dist/', // The base URL appended to the top-level domain (https://brainteasers.herokuapp.com/)
   outputDir: '../static/dist', // The path for where build files will be output, django side
   indexPath: '../../templates/_base_vue.html', // The path for the generated index file
 
