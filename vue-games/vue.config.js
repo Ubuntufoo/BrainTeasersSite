ebd9652490159d8713f71758bd4de04c51b8bb9e
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-29 11:26:02 -0400

    vue config

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: 'brainteasers.herokuapp.com', // The base URL where your app will be deployed
+  publicPath: 'static/dist/', // The base URL where your app will be deployed
   outputDir: '../static/dist', // The path for where build files will be output, django side
   indexPath: '../../templates/_base_vue.html', // The path for the generated index file
 
