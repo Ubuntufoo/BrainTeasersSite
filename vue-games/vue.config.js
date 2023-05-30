44f5fd15c8268c0f1dd671b6ff2420220a8bf042
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-30 16:30:03 -0400

    more style improvements

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   publicPath: '/static/dist/', // The base URL appended to the top-level domain (https://brainteasers.herokuapp.com/)
   outputDir: '../static/dist', // The path for where build files will be output, django side
-  // assetsDir: "../static/dist",
   indexPath: '../../templates/_base_vue.html', // The path for the generated index file
 
   configureWebpack: {
