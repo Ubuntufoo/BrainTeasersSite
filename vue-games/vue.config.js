2ca4cf23e5fcaee9259d8c19cf448af5ea319a73
Timothy Murphy <Timmurphy123@gmail.com>
2023-05-31 11:29:42 -0400

    vue config , removed second static folder

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  publicPath: '/dist/', // The base URL appended to the top-level domain (https://brainteasers.herokuapp.com/)
-  // publicPath: '/static/dist/',
+  publicPath: '/static/dist/', // The base URL appended to the top-level domain (https://brainteasers.herokuapp.com/)
   outputDir: '../static/dist', // The path for where build files will be output, django side
   indexPath: '../../templates/_base_vue.html', // The path for the generated index file
 
