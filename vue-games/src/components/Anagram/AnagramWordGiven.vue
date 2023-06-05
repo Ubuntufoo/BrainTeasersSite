05b4ac20d05a8fcc96692d623b4bc875ee758a01
Timothy Murphy <Timmurphy123@gmail.com>
2023-06-05 16:55:31 -0400

    rebuild, removed styles from anagram startword

--- vue-games/src/components/Anagram/AnagramWordGiven.vue
+++ vue-games/src/components/Anagram/AnagramWordGiven.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex flex-column gap-4 align-items-center m-5">
-      <span class="fs-1 fw-bold text-white rounded-pill bg-primary px-5">{{ startWord }}</span>
+      <span class="">{{ startWord }}</span>
       <span class="fst-italic fs-5">( {{ answersLeft }}  remain )</span>
   </div>
 </template>
