592a976d5f69f6c57b9ef4d5506ffe36f61fa8fa
Timothy Murphy <Timmurphy123@gmail.com>
2023-06-07 13:31:53 -0400

    rebuild, anagramSelectInput @input set to @change

--- vue-games/src/components/Anagram/AnagramSelectInput.vue
+++ vue-games/src/components/Anagram/AnagramSelectInput.vue
@@ -2,7 +2,7 @@
   <div class="d-flex justify-content-evenly mt-3">
     <label class="form-label fw-bold text-uppercase fs-4 pt-3 text-center" for="word-length">Word Length:</label>
     <select class="form-select shadow fs-4 w-50 text-center" role="button" id="word-length" :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)">
+      @change="$emit('update:modelValue', $event.target.value)">
       <option class="fw-bold" v-for="option in options" :key="option[1]" :value="option[1]">
         {{ option[0] }}
       </option>
